8000e208af7f52ce4c1c0a4635e7f4fcc9ea0de4
mrtc0 <kohei.morita@pepabo.com>
2018-06-19 14:51:59 +0900

    タスクの一覧、作成、詳細、編集画面を追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def list
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'tasks/list'
+  get 'tasks/create'
+  get 'tasks/show'
+  get 'tasks/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
