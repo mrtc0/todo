af5a79909377324f0dca0ad9e51ef36b452ed826
mrtc0 <mrtc0@ssrf.in>
2018-07-03 16:42:08 +0900

    不要なコメントを削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    # デフォルトで作成日時の降順で返す
     @tasks = if params[:sort] == 'expire'
                Task.order(expire_at: 'ASC')
              else
