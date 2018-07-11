8b557ea265d92376abbae1e6823e12626e6605fd
mrtc0 <mrtc0@ssrf.in>
2018-07-11 11:11:12 +0900

    インデックスを確認するためのseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,43 @@
 require 'faker'
 
-10.times do |i|
+# Index が効いているか確認するために
+# めちゃくちゃな量を登録している
+# 確認が終わったら削除する
+
+30000.times do |i|
   created_at = Faker::Time.forward.to_datetime
   expire_at = created_at + 2.days
   Task.create(
     title:    "タスク#{i}",
     description:  "内容#{i}",
     priority: 1,
+    status: 0,
+    expire_at: expire_at,
+    created_at: created_at
+  )
+end
+
+30000.times do |i|
+  created_at = Faker::Time.forward.to_datetime
+  expire_at = created_at + 2.days
+  Task.create(
+    title:    "タスク#{i}",
+    description:  "内容#{i}",
+    priority: 1,
+    status: 1,
+    expire_at: expire_at,
+    created_at: created_at
+  )
+end
+
+30000.times do |i|
+  created_at = Faker::Time.forward.to_datetime
+  expire_at = created_at + 2.days
+  Task.create(
+    title:    "タスク#{i}",
+    description:  "内容#{i}",
+    priority: 1,
+    status: 2,
     expire_at: expire_at,
     created_at: created_at
   )
