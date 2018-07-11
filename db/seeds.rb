97285adf9e9382d7bf99a71e60aee42cb8c731a3
mrtc0 <mrtc0@ssrf.in>
2018-07-11 11:31:07 +0900

    別バージョンのseedで確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,40 +4,14 @@ require 'faker'
 # めちゃくちゃな量を登録している
 # 確認が終わったら削除する
 
-30000.times do |i|
+100000.times do |i|
   created_at = Faker::Time.forward.to_datetime
   expire_at = created_at + 2.days
   Task.create(
     title:    "タスク#{i}",
     description:  "内容#{i}",
     priority: 1,
-    status: 0,
-    expire_at: expire_at,
-    created_at: created_at
-  )
-end
-
-30000.times do |i|
-  created_at = Faker::Time.forward.to_datetime
-  expire_at = created_at + 2.days
-  Task.create(
-    title:    "タスク#{i}",
-    description:  "内容#{i}",
-    priority: 1,
-    status: 1,
-    expire_at: expire_at,
-    created_at: created_at
-  )
-end
-
-30000.times do |i|
-  created_at = Faker::Time.forward.to_datetime
-  expire_at = created_at + 2.days
-  Task.create(
-    title:    "タスク#{i}",
-    description:  "内容#{i}",
-    priority: 1,
-    status: 2,
+    status: Faker::Number.between(0, 2),
     expire_at: expire_at,
     created_at: created_at
   )
