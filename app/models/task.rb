8c62be5e2bd0475b0f0a34a51572ef02da2f673c
mrtc0 <mrtc0@ssrf.in>
2018-07-20 12:03:23 +0900

    優先順位でソートできるようにメソッドを生やした

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,6 +11,8 @@ class Task < ApplicationRecord
   scope :filter_by_status, ->(status) { where(status: status) unless status.blank? }
   scope :sort_by_expire, ->(sort) { sort == 'expire' ? order(expire_at: 'ASC') : order(created_at: 'DESC') }
 
+  scope :sort_by_priority, ->(priority) { priority == 'high' ? order(priority: 'DESC') : order(priority: 'ASC') }
+
   def expire_greater_than_current_time
     return if expire_at.nil?
 
@@ -24,4 +26,5 @@ class Task < ApplicationRecord
       .filter_by_status(params[:status])
       .sort_by_expire(params[:sort])
   end
+
 end
