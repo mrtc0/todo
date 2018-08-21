69fe6ce77d594bf0ff1a561ba0aa8a6ac70f0e11
mrtc0 <mrtc0@ssrf.in>
2018-08-21 19:01:30 +0900

    fix factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,28 +2,53 @@ require 'factory_bot'
 require 'faker'
 
 FactoryBot.define do
-  factory :user do
-    email 'user@example.com'
+  # Valid
+  factory :user1, class: User do
+    email 'user1@example.com'
     password 'Password1234'
 
+    after(:create) do |user|
+      FactoryBot.create_list(:user1_task, 3, user: user)
+      FactoryBot.create_list(:status_is_doing_task, 3, user: user)
+      FactoryBot.create_list(:status_is_done_task, 3, user: user)
+    end
+
+    # Valid
+    factory :user2 do
+      email 'user2@example.com'
+      after(:create) do |user|
+        create_list(:user2_task, 3, user: user)
+      end
+    end
+
+    # Invalid
     factory :short_password do
       password 'Pass1'
     end
 
+    # Invalid
     factory :no_number_password do
       password 'password'
     end
 
+    # Invalid
     factory :no_uppercase_in_password do
       password 'password1234'
     end
 
+    # Invalid
     factory :no_lowercase_in_password do
       password 'PASSWORD1234'
     end
 
+    # Invalid
     factory :no_letter_in_password do
       password '12345678'
+    end
+
+    # Invalid
+    factory :invalid_email do
+      email 'email.com'
     end
   end
 end
