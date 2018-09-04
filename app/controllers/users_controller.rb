5ad9ecda61332afe7176f7bc503db7651605ac33
mrtc0 <mrtc0@ssrf.in>
2018-09-04 17:44:44 +0900

    indented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirm)
-  end
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirm)
+    end
 end
