02fd47d02194cdb0e5cfa4d1da2bd751a0909b1c
mrtc0 <mrtc0@ssrf.in>
2018-09-04 12:04:12 +0900

    rails g controller User new create show edit destroy

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  get 'user/create'
+  get 'user/show'
+  get 'user/edit'
+  get 'user/destroy'
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
