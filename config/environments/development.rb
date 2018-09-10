51e601deb0d4e7aaa9cdf83920d8f00ee9c0d65c
mrtc0 <mrtc0@ssrf.in>
2018-09-10 16:00:09 +0900

    delete comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
       Bullet.alert         = true
       Bullet.bullet_logger = true
       Bullet.console       = true
-    # Bullet.growl         = true
       Bullet.rails_logger  = true
       Bullet.add_footer    = true
     end  # Settings specified here will take precedence over those in config/application.rb.
