a9b674712a821b80a2ed01f5e25c41385e373d48
mrtc0 <mrtc0@ssrf.in>
2018-10-01 18:40:28 +0900

    add controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,11 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
   Capybara.javascript_driver = :webkit
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
