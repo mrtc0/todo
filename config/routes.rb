69348446931fc663bcb3831fde364a0e98067376
mrtc0 <mrtc0@ssrf.in>
2018-10-10 08:49:39 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Admin
   namespace :admin do
     get 'users', to: 'users#index'
-    put 'users/edit/:id', to: 'users#update', as: 'user_edit'
+    put 'users/:id', to: 'users#update', as: 'user_edit'
   end
 
   # Signup
